01a4ecfa9c395af69ede9f8b74e094e203af70de
Alexander Daichendt <git@daichendt.one>
2022-08-02 14:44:08 +0200

    feat: add logging

--- src/index.js
+++ src/index.js
@@ -20,16 +20,21 @@ app.post("/", async (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send("No files were uploaded.");
   }
-
+  console.log("Received files:");
+  console.log(req.files);
   let file = req.files.data;
+
+  console.log(`Received file: ${file.name} ${file.mimetype}`);
   const { hash } = await imageHash.hash(file.data, 8, "hex");
   fs.mkdir(`public/${hash}/`, { recursive: true }, (err) => {
     if (err) {
       return console.error(err);
     }
   });
+
   SIZES.forEach((size) => {
     FORMATS.forEach(({ format, options }) => {
+      console.log(`Pushed a task into the queue: ${hash}-${size}.${format}`);
       promises.push(
         sharpStream.clone().resize(size).toFormat(format, options).toFile(`public/${hash}/${hash}-${size}.${format}`)
       );
@@ -37,6 +42,7 @@ app.post("/", async (req, res) => {
   });
   // pipe the received file into the sharp pipeline
   sharp(file.data).pipe(sharpStream);
+  console.log("Starting the conversion tasks...");
 
   const converted = await Promise.all(promises).catch((err) => {
     console.error("Error processing files, let's clean it up", err);
@@ -44,7 +50,7 @@ app.post("/", async (req, res) => {
       fs.rmdirSync(path.join(__dirname, `public/${hash}/`));
     } catch (e) {}
   });
-
+  console.log("Finished processing images.");
   const images = converted.map((conv) => ({ ...conv, href: `/${hash}/${hash}-${conv.width}.${conv.format}` }));
 
   return res.status(200).json(images);
